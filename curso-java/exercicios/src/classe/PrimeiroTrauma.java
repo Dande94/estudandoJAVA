de8ad5813132df4b7f52e764f70522ca1034a921
Anderson Nunes <dande.nunes.94@gmail.com>
2022-12-30 16:18:09 -0300

    Desafio PrimeiroTrauma aulas F098 / F099

---
+++ curso-java/exercicios/src/classe/PrimeiroTrauma.java
@@ -0,0 +1,24 @@
+package classe;
+
+public class PrimeiroTrauma {
+	
+	//minha resposta -> deu certo;
+		int a = 3;
+		static int b = 4;
+		
+	public static void main(String[] args) {
+		
+		PrimeiroTrauma a = new PrimeiroTrauma();
+		
+		System.out.println(a.a);
+		//primeira resposta do professor pro desafio fico iguala minha resolução;
+		
+		//outras respostas do professor
+		System.out.println(b);//Membro de classe pode acessar outro membro de classe, ou static pode acessar outro static;
+		
+		
+		
+		
+	}
+	
+}
