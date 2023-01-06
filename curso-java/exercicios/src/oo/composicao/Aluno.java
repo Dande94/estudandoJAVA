ceb04a027523b51c10078121cda7081327ac3520
Anderson Nunes <dande.nunes.94@gmail.com>
2023-01-06 15:05:27 -0300

    Relação N para N Aula F129 pt.2

--- curso-java/exercicios/src/oo/composicao/Aluno.java
+++ curso-java/exercicios/src/oo/composicao/Aluno.java
@@ -5,15 +5,30 @@ import java.util.List;
 
 public class Aluno {
 	final String nome;
-	List<Curso> cursos = new ArrayList<>();
+	final List<Curso> cursos = new ArrayList<>();//acesso a Classe Curso;
+	//incluindo o 'final' ele trava o endereço de memoria do array, impedindo posteriormente que o mesmo seja redirecionado;
 	
 	Aluno(String nome){
 		this.nome = nome;
 	}
 	
 	void adicionarCurso(Curso curso){
+		//Relação bidirecional	
 		this.cursos.add(curso);
-		curso.alunos.add(this);
-		
+		curso.alunos.add(this);	
 	}
+	
+	Curso obterCursoPorNome(String nome) {
+		for(Curso curso: this.cursos) {
+			if(curso.nome.equalsIgnoreCase(nome)) {
+				return curso;
+			}
+		}
+		return null;
+	}
+	
+	public String toString() {
+		return nome;
+	}
+	
 }
--- curso-java/exercicios/src/oo/composicao/Curso.java
+++ curso-java/exercicios/src/oo/composicao/Curso.java
@@ -4,10 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Curso {
-	String nome;
-	List<Aluno> alunos = new ArrayList<>();
+	final String nome;
+	final List<Aluno> alunos = new ArrayList<>();//acesso a Classe Aluno;
+	//incluindo o 'final' ele trava o endereço de memoria do array, impedindo posteriormente que o mesmo seja redirecionado;
+	
+	Curso(String nome){
+		this.nome= nome;
+	}
 	
 	void adicionarAluno(Aluno aluno) {
+		//Relação bidirecional
 		this.alunos.add(aluno);
 		aluno.cursos.add(this);
 	}
