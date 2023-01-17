ba2cc7851595c5b34fe4c3c0579fe6ca9a8c641d
Anderson Nunes <dande.nunes.94@gmail.com>
2023-01-17 15:49:10 -0300

    Herança na prática Aula F139 pt.2

--- curso-java/exercicios/src/oo/heranca/Heroi.java
+++ curso-java/exercicios/src/oo/heranca/Heroi.java
@@ -2,4 +2,27 @@ package oo.heranca;
 
 public class Heroi extends Jogador{
 	//transmissão de atributos e comportamentos vinda da classe Jogador;
+	
+	boolean atacar(Jogador oponente) {
+		
+		//sobrescrever;
+//		int deltaX = Math.abs(x - oponente.x);
+//		int deltaY = Math.abs(y - oponente.y);
+//		
+//		if(deltaX == 0 && deltaY == 1) {//verifica se estão um ao lado do outro, heroi e monstro
+//			oponente.vida -= 10;
+//			return true;
+//		}else if(deltaX == 1 && deltaY == 0) {//verifica se estão um ao lado do outro, heroi e monstro
+//			oponente.vida -= 10;			
+//			return true;
+//		}else {
+//			return false;
+//		}
+		
+		
+		
+		boolean ataque1 = super.atacar(oponente);//super. -> reutilizar u metodo da superclasse(classe pai);
+		boolean ataque2 = super.atacar(oponente);
+		return ataque1 || ataque2;
+	}
 }
